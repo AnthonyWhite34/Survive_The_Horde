ab3109d3dac9f3ab80c7238b0351e1f0e057e8cc
Anthony Williams-White <140553269+AnthonyWhite34@users.noreply.github.com>
2026-05-26 15:29:57 -0400

    Add MyInputComponent and EnhancedInput module
    Introduce UMyInputComponent (header + implementation) with a templated BindabilityActions helper that iterates UMyInputConfig::AbilityInputActions and binds InputAction entries to Pressed/Released/Held callbacks using ETriggerEvent mapping. Update Survive_The_Horde.uproject to add the EnhancedInput module dependency. Also update BP_PlayerCharacter.uasset (LFS oid/size changed).

---
+++ Source/Survive_The_Horde/Public/Input/MyInputComponent.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "MyInputConfig.h"
+#include "MyInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVIVE_THE_HORDE_API UMyInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+	//create a function that we can use on our Player controller so we can bind callbacks. and i want the callbacks to be paramaters to those callbacks
+	template<class UserClass, typename PressedFuncType, typename ReleaseFuncType, typename HeldFuncType>
+	void BindabilityActions(const UMyInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleaseFuncType ReleasedFunc, HeldFuncType HeldFunc);
+	
+	
+};
+
+template <class UserClass, typename PressedFuncType, typename ReleaseFuncType, typename HeldFuncType>
+void UMyInputComponent::BindabilityActions(const UMyInputConfig* InputConfig, UserClass* Object,
+	PressedFuncType PressedFunc, ReleaseFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig);
+	
+	for (const FMyInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			
+			if (PressedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Started, Object, PressedFunc, Action.InputTag);
+			}
+			
+			if (ReleasedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag);
+			}
+			
+			if (HeldFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Action.InputTag);
+			}
+			
+		}
+	}
+}
